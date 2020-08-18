741e855c52a4e1746bff8b025f813db3112b217d
deepagitmca <48791909+deepagitmca@users.noreply.github.com>
2020-08-18 23:39:56 +0530

    Add files via upload

---
+++ array left rotation.py
@@ -0,0 +1,19 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+   
+    new_arr =a[d:]+a[0:d]
+    print(*new_arr)
